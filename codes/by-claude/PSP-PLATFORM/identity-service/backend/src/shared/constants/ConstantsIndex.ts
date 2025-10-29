d09248c15625499e54c4155883d1888ba6de0ed3
thanhhaunv <thanhhaunv@gmail.com>
2025-10-29 14:01:23 +0700

    Create ConstantsIndex.ts

---
+++ codes/by-claude/PSP-PLATFORM/identity-service/backend/src/shared/constants/ConstantsIndex.ts
@@ -0,0 +1,9 @@
+/**
+ * Shared Constants - Index
+ * 
+ * Export all constants for easy importing
+ * Usage: import { ErrorMessages, SystemConstants } from '../shared/constants'
+ */
+
+export * from './ErrorMessages';
+export * from './SystemConstants';
